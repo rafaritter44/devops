b3165d2914dd38d3e10f6ab0f5acf5c485e8c1d9
Rafael Ritter <rafaritter44@hotmail.com>
2019-12-22 22:05:34 -0300

    Solve Terraform AWS EC2 script bug

--- terraform/aws/ec2/main.tf
+++ terraform/aws/ec2/main.tf
@@ -25,6 +25,22 @@ resource "aws_security_group" "web_sg" {
             "0.0.0.0/0"
         ]
     }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+    }
     tags = {
         Name = "web_sg"
     }
