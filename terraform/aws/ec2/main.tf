ec7e79116e2fccc9c69ccf40a50d19536c787553
Rafael Ritter <rafaritter44@hotmail.com>
2019-12-22 21:38:34 -0300

    Refactor Terraform AWS EC2 script

--- terraform/aws/ec2/main.tf
+++ terraform/aws/ec2/main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "amazon_linux_2" {
     filter {
         name = "name"
         values = [
-            "amzn2-ami-hvm-2.0.*-arm64-gp2"
+            "amzn2-ami-hvm-2.0.*-x86_64-gp2"
         ]
     }
     most_recent = true
@@ -21,23 +21,33 @@ resource "aws_security_group" "web_sg" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
     }
     tags = {
         Name = "web_sg"
     }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "web" {
     ami = data.aws_ami.amazon_linux_2.id
     instance_type = "t2.micro"
-    security_groups = [
+    vpc_security_group_ids = [
         aws_security_group.web_sg.id
     ]
+    key_name = aws_key_pair.deployer.key_name
     user_data = file("bootstrap.sh")
     tags = {
         Name = "web"
     }
     depends_on = [
-        aws_security_group.web_sg
+        aws_security_group.web_sg,
+        aws_key_pair.deployer
     ]
 }
