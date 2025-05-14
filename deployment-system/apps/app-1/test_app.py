5ae7518d31e2989cbf38f75beaca0ddc61d02aa8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-05-14 16:58:16 -0300

    Implement tests for app-1

---
+++ deployment-system/apps/app-1/test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_root(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello!" in response.data
+
+def test_metrics(client):
+    response = client.get('/metrics')
+    assert response.status_code == 200
+    assert b"flask_app_request_count" in response.data
