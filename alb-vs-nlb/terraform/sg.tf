0878ceb3100f4c321f051ba57dde5d99753d5416
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-08-07 14:19:35 -0300

    Implement sg.tf

---
+++ alb-vs-nlb/terraform/sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "app_sg" {
+  name        = "app_sg"
+  description = "Allow HTTP (5000), TCP (6000), and SSH"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6000
+    to_port     = 6000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
