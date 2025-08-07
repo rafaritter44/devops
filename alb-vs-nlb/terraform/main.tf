7513e702bf0b42ec2ca86fdbae03c68e64137225
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-08-07 14:02:58 -0300

    Add another subnet

--- alb-vs-nlb/terraform/main.tf
+++ alb-vs-nlb/terraform/main.tf
@@ -6,10 +6,17 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "subnet_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "sa-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "sa-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -26,6 +33,11 @@ resource "aws_route_table" "rt" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.subnet_b.id
   route_table_id = aws_route_table.rt.id
 }
