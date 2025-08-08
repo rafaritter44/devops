dbb3eb0727a03229340d396ef419df3f6302361c
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-08-08 14:12:35 -0300

    Fix ALB target groups

--- alb-vs-nlb/terraform/alb.tf
+++ alb-vs-nlb/terraform/alb.tf
@@ -6,8 +6,25 @@ resource "aws_lb" "alb" {
   security_groups    = [aws_security_group.app_sg.id]
 }
 
-resource "aws_lb_target_group" "alb_tg" {
-  name     = "alb-tg-http"
+resource "aws_lb_target_group" "alb_tg_a" {
+  name     = "alb-tg-http-a"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    port                = "5000"
+    protocol            = "HTTP"
+    path                = "/health"
+    matcher             = "200"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+  }
+}
+
+resource "aws_lb_target_group" "alb_tg_b" {
+  name     = "alb-tg-http-b"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
@@ -25,13 +42,13 @@ resource "aws_lb_target_group" "alb_tg" {
 
 resource "aws_lb_target_group_attachment" "alb_http_a" {
   count            = 2
-  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_group_arn = aws_lb_target_group.alb_tg_a.arn
   target_id        = aws_instance.http_server_a[count.index].id
   port             = 5000
 }
 
 resource "aws_lb_target_group_attachment" "alb_http_b" {
-  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_group_arn = aws_lb_target_group.alb_tg_b.arn
   target_id        = aws_instance.http_server_b.id
   port             = 5000
 }
@@ -42,7 +59,7 @@ resource "aws_lb_listener" "alb_listener" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_tg.arn
+    target_group_arn = aws_lb_target_group.alb_tg_a.arn
   }
 }
 
